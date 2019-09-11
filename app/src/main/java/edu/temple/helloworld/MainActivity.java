03251e860e7da9a5b5434de566bef6fc9cdf2838
xinwencui <c.xw3216@gmail.com>
2019-09-11 14:35:08 -0400

    second commit onClick method

---
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -0,0 +1,28 @@
+package edu.temple.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        Toast.makeText(this, "You have clicked the button", Toast.LENGTH_SHORT).show();
+
+    }
+
+}
